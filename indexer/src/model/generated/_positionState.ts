b23425f7be77ab9d275d231e6b64a9062a2e9ecd
Anukul Pandey <anukul.030601@gmail.com>
2025-03-13 15:49:23 +0530

    feat: separated indexer

--- indexer/src/model/generated/_positionState.ts
+++
@@ -1,8 +0,0 @@
-export enum PositionState {
-    Available = "Available",
-    RegularSale = "RegularSale",
-    Auction = "Auction",
-    Raffle = "Raffle",
-    Loan = "Loan",
-    Deleted = "Deleted",
-}
